fd5854d923fd11b966d59ac8fb82cc343e6b3d9d
Mikhail Chairkin <mchvladim90@gmail.com>
2024-11-20 19:35:30 +0500

    Normalize line endings

---
+++ Source/CryptRIder/CryptRIder.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CryptRIder.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CryptRIder, "CryptRIder" );
+ 
---
+++ Source/CryptRIder/CryptRIderGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CryptRIderGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACryptRIderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACryptRIderGameMode();
+};
+
+
+
